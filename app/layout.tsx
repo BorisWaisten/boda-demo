84801c268992dac9f212d59244d7e4996451bbcb
BorisWaisten <104634915+BorisWaisten@users.noreply.github.com>
2026-03-18 20:32:42 -0300

    fix:arreglo metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,21 +4,6 @@ import Footer from '../components/Footer'
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://invitacion-boda.vercel.app'),
-  title: 'Invitación de Boda ',
-  description: 'Te invitamos a celebrar nuestra boda',
-  keywords: 'boda, invitación, matrimonio, celebración',
-  openGraph: {
-    title: 'Invitación de Boda ',
-    description: 'Te invitamos a celebrar nuestra boda',
-    type: 'website',
-    images: ['/16_14424.webp'],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'Invitación de Boda',
-    description: 'Te invitamos a celebrar nuestra boda',
-    images: ['/16_14424.webp'],
-  },
 }
 
 export default function RootLayout({
